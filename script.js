3d248790f124dfa08ae5faa7199ba6bac9baa528
glrm079 <glrm07928@gmail.com>
2024-02-02 17:11:27 -0300

    add”
    git commit -m add”

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById('openOne').addEventListener('click', ()=>{
+    document.getElementById('openOne').classList.toggle('hide')
+    document.getElementById('hidenOne').classList.toggle('hide')
+    document.getElementById('hidenItenOne').classList.toggle('hide')
+})
+document.getElementById('hidenOne').addEventListener('click', ()=>{
+    document.getElementById('openOne').classList.toggle('hide')
+    document.getElementById('hidenOne').classList.toggle('hide')
+    document.getElementById('hidenItenOne').classList.toggle('hide')
+})
+
+
+document.getElementById('openTwo').addEventListener('click', ()=>{
+    document.getElementById('openTwo').classList.toggle('hide')
+    document.getElementById('hidenTwo').classList.toggle('hide')
+    document.getElementById('hidenItenTwo').classList.toggle('hide')
+})
+document.getElementById('hidenTwo').addEventListener('click', ()=>{
+    document.getElementById('openTwo').classList.toggle('hide')
+    document.getElementById('hidenTwo').classList.toggle('hide')
+    document.getElementById('hidenItenTwo').classList.toggle('hide')
+})
+
+
+document.getElementById('openTree').addEventListener('click', ()=>{
+    document.getElementById('openTree').classList.toggle('hide')
+    document.getElementById('hidenTree').classList.toggle('hide')
+    document.getElementById('hidenItenTree').classList.toggle('hide')
+})
+document.getElementById('hidenTree').addEventListener('click', ()=>{
+    document.getElementById('openTree').classList.toggle('hide')
+    document.getElementById('hidenTree').classList.toggle('hide')
+    document.getElementById('hidenItenTree').classList.toggle('hide')
+})
+
+document.getElementById('openFour').addEventListener('click', ()=>{
+    document.getElementById('openFour').classList.toggle('hide')
+    document.getElementById('hidenFour').classList.toggle('hide')
+    document.getElementById('hidenItenFour').classList.toggle('hide')
+})
+document.getElementById('hidenFour').addEventListener('click', ()=>{
+    document.getElementById('openFour').classList.toggle('hide')
+    document.getElementById('hidenFour').classList.toggle('hide')
+    document.getElementById('hidenItenFour').classList.toggle('hide')
+})
